c435901868f983d088a7a0513a5ed4330743c1d0
ggyaal <ggyaal@gmail.com>
2022-03-14 16:23:21 +0900

    setting test 1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          안녕하세용 <code>typescript기반 react화면입니다ㅎ</code>모두들 안녕!
         </p>
         <a
           className="App-link"
